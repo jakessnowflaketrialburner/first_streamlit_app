943668b93e3465f912d3c0b310f545b262dabc24
jakessnowflaketrialburner <113474801+jakessnowflaketrialburner@users.noreply.github.com>
2022-09-21 11:46:13 -0500

    Update streamlit_app.py
    stopped streamlit at snowflake step

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 streamlit.title('my parents new healthy diner')
 
 streamlit.header('Breakfast Favorites')
@@ -27,10 +29,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+#chill for now
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
